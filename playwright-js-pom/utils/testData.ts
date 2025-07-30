0be59b3d779611a808c54e05b867b606a6d523bb
ritvikvedarth-0708 <64010752+ritvikvedarth-0708@users.noreply.github.com>
2025-07-30 12:07:35 +0530

    Add generated Playwright POM test suite

---
+++ playwright-js-pom/utils/testData.ts
@@ -0,0 +1,8 @@
+export const testData = {
+  validUsername: 'peter@nestle.com',
+  validPassword: 'Scb@1234',
+  invalidUsername: 'invalid@example.com',
+  invalidPassword: 'InvalidPassword',
+  eligibleBatchId: 'eligible-batch-id',
+  invalidVesselIdBatch: 'invalid-vessel-id-batch',
+};
